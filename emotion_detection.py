8dd4889b911471359aadc5fd6c1e7b2d394051cb
manuelfhinojosa <ingmanuelhinojosa@gmail.com>
2025-12-18 20:23:13 -0500

    Task 3 completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,11 +7,11 @@ def emotion_detector(text_to_analyse):  # Define a function
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API
     formatted_response = json.loads(response.text) #Formatting response
-    anger_score = formatted_response['emotionPredictions']['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions']['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions']['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions']['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions']['emotion']['sadness']
-    emotions = {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score}
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
     dominant_emotion = max(emotions, key = emotions.get)
     return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant_emotion} # Return the response
